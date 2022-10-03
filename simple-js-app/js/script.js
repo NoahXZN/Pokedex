93b94afef54d660cbc63b05d24d722cba500b59e
NoahXZN <113633351+NoahXZN@users.noreply.github.com>
2022-10-03 20:15:57 +0200

    Update script.js

--- simple-js-app/js/script.js
+++ simple-js-app/js/script.js
@@ -1,5 +1,5 @@
 let pokemonList = [
   {name: 'Aron', height: 4, type: ['steel', 'rock']},
   {name: 'Mudkip', height: 4, type: 'water'},
-  {name: 'Bulbasaur', height: 7; type: ['grass', 'poison']}
+  {name: 'Bulbasaur', height: 7, type: ['grass', 'poison']}
 ];
