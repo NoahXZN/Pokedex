6a42738f7061f139caaf10b7ada36de1e1a79ffd
NoahXZN <113633351+NoahXZN@users.noreply.github.com>
2022-10-05 19:37:06 +0200

    Update script.js

--- simple-js-app/js/script.js
+++ simple-js-app/js/script.js
@@ -3,3 +3,14 @@ let pokemonList = [
   {name: 'Mudkip', height: 4, type: 'water'},
   {name: 'Bulbasaur', height: 7, type: ['grass', 'poison']}
 ];
+
+//Lets user know pokemon type and measurments
+for (let i = 0;
+  i < pokemonList.length; i++){
+    if pokemonList[i].height >= 7) {
+      document.write(pokemonList[i].name + " is " + pokemonList[i].height + " meters tall and is " + pokemonList[i].type + "type. What a big pokemon!<br>")
+    }
+    else {
+      document.write(pokemonList[i].name + " is " + pokemonList[i].height + " meters tall and is " + pokemonList[i].type + "type. Great catch!<br>")
+    }
+  }
