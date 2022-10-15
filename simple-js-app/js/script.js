ffe286ccfb144330a5dc468bf5d2790f32a72ef7
NoahXZN <113633351+NoahXZN@users.noreply.github.com>
2022-10-15 10:42:45 +0200

    Update script.js

--- simple-js-app/js/script.js
+++ simple-js-app/js/script.js
@@ -1,27 +1,57 @@
-let pokemonList (function()=
- let repository =[
-  {name: 'Aron', height: 4, type: ['steel' , 'rock']},
-  {name: 'Mudkip', height: 4, type: 'water'},
-  {name: 'Bulbasaur', height: 7, type: ['grass' , 'poison']}
-];
+let pokemonRepository = (function() {
+  let repositoryList = [{
+      name: 'Aron',
+      height: 4,
+      type: ['steel', 'rock']
+    },
+    {
+      name: 'Mudkip',
+      height: 4,
+      type: 'water'
+    },
+    {
+      name: 'Bulbasaur',
+      height: 7,
+      type: ['grass', 'poison']
+    }
+  ];
 
-function getAll () {
-  return repository;
-}
+  function getAll() {
+    return repositoryList;
+  }
 
-function add(pokemon) {
-  repository.push(pokemon);
-}
+  function add(pokemon) {
+    repository.push(pokemon);
+  }
 
-function showDetails(pokemon){
-  console.log(pokemon.name)
-}
+  function showDetails(pokemon) {
+    console.log(pokemon.name)
+  }
 
-console.log(pokemonRepository.getAll());
+  return {
+    getAll,
+    add,
+    showDetails,
+  }
+}());
 
-pokemonRepository.getAll().forEach(function(pokemon){
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.getAll().forEach(function(pokemon) {
+  if (pokemon.height >= 7) {
+    document.write(pokemon.name + " is " + pokemon.height + " meters tall and is " + pokemon.type + " type. What a big pokemon!<br>")
+  } else {
+    document.write(pokemon.name + " is " + pokemon.height + " meters tall and is " + pokemon.type + " type. Great catch!<br>")
+  }
 })
+
+//Lets user know pokemon type and measurments
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height >= 7) {
+    document.write(pokemonList[i].name + " is " + pokemonList[i].height + " meters tall and is " + pokemonList[i].type + " type. What a big pokemon!<br>")
+  } else {
+    document.write(pokemonList[i].name + " is " + pokemonList[i].height + " meters tall and is " + pokemonList[i].type + " type. Great catch!<br>")
+  }
+}
+
 
 //Lets user know pokemon type and measurments
 for (let i = 0;
